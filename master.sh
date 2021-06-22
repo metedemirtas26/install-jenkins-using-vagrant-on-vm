708d6eb245b8737edd4c24360a54fff3e9666c6e
Mete Demirtas <metedemirtas26@gmail.com>
2021-06-22 13:27:14 +0200

    Create master.sh

---
+++ master.sh
@@ -0,0 +1,15 @@
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > \
+    /etc/apt/sources.list.d/jenkins.list'
+
+sudo apt-get update
+
+sudo apt-get install jenkins -y
+
+sudo systemctl daemon-reload
+
+sudo systemctl start 
+
+# Jenkins Password for Login
+cat /var/lib/jenkins/secrets/initialAdminPassword
+
